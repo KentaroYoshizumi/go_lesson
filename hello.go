9a02bb0f57ba7db5233f2acbca0a0a162ee8a122
KentaroYoshizumi <schecter3.2.1-@softbank.ne.jp>
2025-09-06 23:43:30 +0400

    learned function in Go

--- hello.go
+++ hello.go
@@ -2,17 +2,12 @@ package main
 
 import "fmt"
 
-// var (
-// 	u8  uint8     = 255
-// 	i8  int8      = 127
-// 	f32 float32   = 0.2
-// 	c64 complex64 = -5 + 12i
-// )
-
 func main() {
-	fmt.Println(10 / 2)
-	fmt.Println(1 + 1)
-	fmt.Println(10 - 3)
-	fmt.Println(10 / 3)
-	// fmt.Printf("%T %v", u8, u8)
+	var x int = 1
+	xx := float64(x)
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
+
+	var y float64 = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d\n", yy, yy, yy)
 }
